3bf899a1e67ade822d37adfe0dc3daad54e18364
syanasaber <59959762+syanasaber@users.noreply.github.com>
2021-08-04 16:44:45 +0900

    Add files via upload

---
+++ index_files/app.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+
+    /**
+     * バーガーメニュー展開
+     */
+    var burger = $('.burger');
+    var menu = $('#' + burger.data('target'));
+    burger.on('click', function() {
+        burger.toggleClass('is-active');
+        menu.toggleClass('is-active');
+    });
+
+    /**
+     * 操作パネル展開
+     */
+    var panel_button = $('.panel_button');
+    var panel = $('#panel');
+    panel_button.on('click', function() {
+        panel.toggleClass('is-hidden');
+    });
+
+    /**
+     * 汎用モーダル表示制御
+     * 
+     * 参考
+     * https://blog.narito.ninja/detail/131
+     */
+    for (const element of document.querySelectorAll('.modal .close-modal, .show-modal')) {
+        element.addEventListener('click', e => {
+            const modalId = element.dataset.target;
+            const modal = document.getElementById(modalId);
+            modal.classList.toggle('is-active');
+        });
+    }
+
+    /**
+     * テーブルクリックで色を付ける
+     */
+    $("#data_table tr").on("click",function(){
+        if ($(this).hasClass("is-selected")) {
+            $(this).removeClass("is-selected");
+        } else {
+            $(this).addClass("is-selected");
+        }
+    });
+var txt = "";
+    /**
+     * リストの情報取得
+     */
+    $("#data_list tbody tr").on("click",function(){
+        var text = $(this).children().text().trim();
+        console.log(text);
+        txt = text;
+        $("#edit_area").val(text);
+    });
+
+    $.contextMenu({
+        selector: '#data_list tbody', 
+        callback: function(key, options) {
+            var m = "clicked: " + key;
+            window.console && console.log(m) || alert(m + txt); 
+        },
+        items: {
+            "edit": {name: "Edit", icon: "edit"},
+            "cut": {name: "Cut", icon: "cut"},
+            copy: {name: "Copy", icon: "copy"},
+            "paste": {name: "Paste", icon: "paste"},
+            "delete": {name: "Delete", icon: "delete"},
+            "sep1": "---------",
+            "quit": {name: "Quit", icon: function(){
+                return 'context-menu-icon context-menu-icon-quit';
+            }}
+        }
+    });
+
+    $('.context-menu-one').on('click', function(e){
+        console.log('clicked', this);
+    })    
+});
