5ad3b98d579556a803d1318ea34cad69108754c3
syanasaber <59959762+syanasaber@users.noreply.github.com>
2021-08-13 18:32:44 +0900

    Update app.js

--- project.html_files/app.js
+++ project.html_files/app.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
     $("#test").on("click",function(){
       alert('てすと');
       $('#testDialog').removeClass("d-none");
-    }
+    });
 
     /**
      * テーブルクリックで色を付ける
